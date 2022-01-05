9e871e6680c594827086db1f46ad420e4db33dba
qiushaocloud <qiushaocloud@126.com>
2022-01-05 19:37:04 +0800

    update code

--- web-js/qiushaositecounter.js
+++ web-js/qiushaositecounter.js
@@ -1,5 +1,6 @@
 (function(){
   var CUSTOM_HOST = 'www.qiushaocloud.top';
+  var SAVE_SESSION_DURATION = 1 * 60 * 60 * 1000;
 
   function reqSiteCounterAPI (
     siteMd5,
@@ -40,11 +41,60 @@
   }
   
   var apiResult = {};
+  var isIncredSite = false;
+  var isIncredSitePage = false;
+  var timer = undefined;
+  var currCheckCount = 0;
   
-  var timer = setInterval(function() {
+  timer = setInterval(function() {
+    currCheckCount++;
+
     var sitePvEle = document.getElementById('qiushaositecounter_value_site_pv');
     var siteUvEle = document.getElementById('qiushaositecounter_value_site_uv');
     var todaySitePvEle = document.getElementById('qiushaositecounter_value_today_site_pv');
     var todaySiteUvEle = document.getElementById('qiushaositecounter_value_today_site_uv');
-  }, interval);
+    
+    var sitePagePvEle = document.getElementById('qiushaositecounter_value_site_page_pv');
+    var sitePageUvEle = document.getElementById('qiushaositecounter_value_site_page_uv');
+    var todaySitePagePvEle = document.getElementById('qiushaositecounter_value_today_site_page_pv');
+    var todaySitePageUvEle = document.getElementById('qiushaositecounter_value_today_site_page_uv');
+
+    var hasSiteEle = !!(sitePvEle || siteUvEle || todaySitePvEle || todaySiteUvEle);
+    var hasSitePageEle = !!(sitePagePvEle || sitePageUvEle || todaySitePagePvEle || todaySitePageUvEle);
+
+    if ((isIncredSite && isIncredSitePage) || currCheckCount > 180) {
+      timer && clearInterval(timer);
+      timer = undefined;
+      return;
+    }
+
+    var nowTs = Date.now();
+    var saveSiteTs = window.localStorage.getItem('qiushaositecounter_session_save_ts');
+    var isHistroySession = false;
+    if (saveSiteTs && (nowTs - Number(saveSiteTs)) < SAVE_SESSION_DURATION)
+      isHistroySession = true;
+
+    if (hasSiteEle || hasSitePageEle) {
+      var siteMd5 = window.location.host;
+      var sitePageMd5 = siteMd5+'/'+window.location.pathname;
+      var isIncrSite = !isIncredSite;
+
+      isIncredSite = true;
+
+      reqSiteCounterAPI(
+        siteMd5,
+        sitePageMd5,
+        isIncrSite,
+        isHistroySession,
+        function (err, res) {
+          if (err) {
+            console.error('reqSiteCounterAPI err:', err);
+            return;
+          }
+          
+          apiResult = res;
+        }
+      );
+    }
+  }, 1000);
 })();
